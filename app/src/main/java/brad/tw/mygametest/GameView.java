e08bace246682c51412e56a28b03d532c0b70a03
Brad <brad@brad.tw>
2016-10-01 14:29:34 +0800

    v7

--- app/src/main/java/brad/tw/mygametest/GameView.java
+++ app/src/main/java/brad/tw/mygametest/GameView.java
@@ -9,8 +9,10 @@ import android.graphics.Color;
 import android.graphics.Matrix;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.LinkedList;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -22,10 +24,13 @@ public class GameView extends View {
     private boolean isInit; // false
     private int viewW, viewH;   // 0
     private Bitmap ballBmp;
-    private Ball ball;
-    private float ballW, ballH, ballX, ballY, dx, dy;   // 0.0f
+    private float ballW, ballH;   // 0.0f
     private Matrix matrix;
     private Timer timer;
+    private LinkedList<Ball> balls;
+    private int[] ballImgs = {R.drawable.ball, R.drawable.ball1,
+    R.drawable.ball2, R.drawable.ball3};
+    private Bitmap[] bmps = new Bitmap[ballImgs.length];
 
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -33,6 +38,7 @@ public class GameView extends View {
         res = context.getResources();
         matrix = new Matrix();
         timer = new Timer();
+        balls = new LinkedList<>();
 
         setBackgroundResource(R.drawable.bg);
 
@@ -43,16 +49,25 @@ public class GameView extends View {
         viewW = getWidth(); viewH = getHeight();
         ballW = viewW / 12f; ballH = ballW;
 
-        dx = viewW / 240f; dy = viewH / 180f;
-
-        ballBmp = BitmapFactory.decodeResource(res, R.drawable.ball);
-        ballBmp = resizeBitmap(ballBmp, ballW, ballH);
-        ball = new Ball(0,0,ballBmp);
+        for (int i=0; i<ballImgs.length; i++) {
+            Bitmap tempBmp = BitmapFactory.decodeResource(res, ballImgs[i]);
+            tempBmp = resizeBitmap(tempBmp, ballW, ballH);
+            bmps[i] = tempBmp;
+        }
 
         timer.schedule(new RefreshTask(), 0, 40);
-        timer.schedule(ball, 0, 30);
 
         isInit = true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Ball ball = new Ball(
+                event.getX()-ballW/2, event.getY()-ballH/2,
+                bmps[(int)(Math.random()*bmps.length)]);
+        timer.schedule(ball, 0, 30 + (int)(Math.random()*70));
+        balls.add(ball);
+        return false;
     }
 
     @Override
@@ -60,7 +75,9 @@ public class GameView extends View {
         super.onDraw(canvas);
         if (!isInit) init();
 
-        canvas.drawBitmap(ball.bmp,ball.x,ball.y,null);
+        for (Ball ball : balls) {
+            canvas.drawBitmap(ball.bmp, ball.x, ball.y, null);
+        }
 
     }
 
@@ -71,9 +88,10 @@ public class GameView extends View {
         }
     }
     private class Ball extends TimerTask {
-        float x, y;
+        float x, y, dx, dy;
         Bitmap bmp;
         Ball(float x, float y, Bitmap bmp){
+            dx = viewW / 120f; dy = viewH / 90f;
             this.x = x; this.y = y;
             this.bmp = bmp;
         }
