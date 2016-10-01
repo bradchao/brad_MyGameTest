544611a8c6c4e722e22ff808c3a48c726ce2623d
Brad <brad@brad.tw>
2016-10-01 11:56:55 +0800

    v2

--- app/src/main/java/brad/tw/mygametest/GameView.java
+++ app/src/main/java/brad/tw/mygametest/GameView.java
@@ -1,6 +1,10 @@
 package brad.tw.mygametest;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
@@ -9,9 +13,20 @@ import android.view.View;
  * Created by user on 2016/10/1.
  */
 public class GameView extends View {
+    private Resources res;
 
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setBackgroundColor(Color.YELLOW);
+        res = context.getResources();
+        setBackgroundResource(R.drawable.bg);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Bitmap bg = BitmapFactory.decodeResource(res, R.drawable.bg);
+        canvas.drawBitmap(bg,-200,0,null);
+
     }
 }
