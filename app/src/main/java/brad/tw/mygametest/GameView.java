b204b77a7303a6ae248671fd1de69c10db66cfe1
Brad <brad@brad.tw>
2016-10-01 13:36:00 +0800

    v4

--- app/src/main/java/brad/tw/mygametest/GameView.java
+++ app/src/main/java/brad/tw/mygametest/GameView.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -17,10 +18,16 @@ public class GameView extends View {
     private Resources res;
     private boolean isInit; // false
     private int viewW, viewH;
+    private Bitmap ball;
+    private float ballW, ballH;
+    private Matrix matrix;
 
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
         res = context.getResources();
+        matrix = new Matrix();
+
         setBackgroundResource(R.drawable.bg);
 
 
@@ -28,6 +35,10 @@ public class GameView extends View {
 
     private void init(){
         viewW = getWidth(); viewH = getHeight();
+        ballW = viewW / 12f; ballH = ballW;
+
+        ball = BitmapFactory.decodeResource(res, R.drawable.ball);
+        ball = resizeBitmap(ball, ballW, ballH);
 
         isInit = true;
     }
@@ -37,8 +48,17 @@ public class GameView extends View {
         super.onDraw(canvas);
         if (!isInit) init();
 
-        Bitmap ball = BitmapFactory.decodeResource(res, R.drawable.ball);
         canvas.drawBitmap(ball, 0,0,null);
+
+    }
+
+    private Bitmap resizeBitmap(Bitmap src, float newW, float newH){
+        matrix.reset();
+        matrix.postScale(newW/src.getWidth(),newH/src.getHeight());
+        Bitmap newBmp = Bitmap.createBitmap(src,
+                0,0,src.getWidth(),src.getHeight(),
+                matrix,false);
+        return newBmp;
 
     }
 }
