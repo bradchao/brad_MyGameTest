045eb1db01d397769bab396b769f2556cb65e6ae
Brad <brad@brad.tw>
2016-10-01 13:49:17 +0800

    v5

--- app/src/main/java/brad/tw/mygametest/GameView.java
+++ app/src/main/java/brad/tw/mygametest/GameView.java
@@ -11,22 +11,27 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by user on 2016/10/1.
  */
 public class GameView extends View {
     private Resources res;
     private boolean isInit; // false
-    private int viewW, viewH;
+    private int viewW, viewH;   // 0
     private Bitmap ball;
-    private float ballW, ballH;
+    private float ballW, ballH, ballX, ballY, dx, dy;   // 0.0f
     private Matrix matrix;
+    private Timer timer;
 
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         res = context.getResources();
         matrix = new Matrix();
+        timer = new Timer();
 
         setBackgroundResource(R.drawable.bg);
 
@@ -37,8 +42,12 @@ public class GameView extends View {
         viewW = getWidth(); viewH = getHeight();
         ballW = viewW / 12f; ballH = ballW;
 
+        dx = viewW / 240f; dy = viewH / 180f;
+
         ball = BitmapFactory.decodeResource(res, R.drawable.ball);
         ball = resizeBitmap(ball, ballW, ballH);
+
+        timer.schedule(new BallTask(), 1*1000, 70);
 
         isInit = true;
     }
@@ -48,8 +57,17 @@ public class GameView extends View {
         super.onDraw(canvas);
         if (!isInit) init();
 
-        canvas.drawBitmap(ball, 0,0,null);
+        canvas.drawBitmap(ball,ballX,ballY,null);
 
+    }
+
+    private class BallTask extends TimerTask {
+        @Override
+        public void run() {
+            ballX += dx;
+            ballY += dy;
+            postInvalidate();
+        }
     }
 
     private Bitmap resizeBitmap(Bitmap src, float newW, float newH){
